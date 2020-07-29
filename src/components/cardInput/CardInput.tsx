512d5aa14d6c99d0b265da99658d3249abdfc6b8
Marcin Stefaniak <m.stefaniak@tidio.net>
2020-07-29 07:54:42 +0200

    Add all required features to component

--- src/components/cardInput/CardInput.tsx
+++ src/components/cardInput/CardInput.tsx
@@ -2,49 +2,119 @@ import React, { ChangeEvent, useState, useRef } from 'react';
 import Payment from 'payment';
 import styled from '@emotion/styled';
 
+import cardLogos from './logos';
+
 const Wrapper = styled.div({
     display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     margin: 30,
     borderRadius: 4,
     backgroundColor: '#fff',
     padding: '5px 10px',
-    width: 300,
+    width: 360,
 });
 
-const Input = styled.input({
-    border: 'none',
+const Input = styled.input(({ width = 180, isError = false }: { width?: number, isError?: boolean }) => ({
+    border: isError ? '1px solid #ff0033' : '1px solid #fff',
     outline: 'none',
     height: 35,
     fontSize: 16,
+    width,
+    padding: '0 5px',
+}));
+
+const CardLogo = styled.img({
+    width: 35,
+    height: 23,
+    marginRight: 8,
 });
+
+type HandledCardTypes = 'placeholder' | 'visa' | 'mastercard' | 'amex';
 
 const CardInput = (): JSX.Element => {
     const [cardNumber, setCardNumber] = useState('');
+    const [cardLogo, setCardLogo] = useState(cardLogos.placeholder);
     const [expiry, setExpiry] = useState('');
+    const [expiryValid, setExpiryValid] = useState(true);
+    const [cvc, setCvc] = useState('');
     const expiryRef = useRef<HTMLInputElement>(null);
+    const cvcRef = useRef<HTMLInputElement>(null);
 
     const onCardNumberChange = (event: ChangeEvent<HTMLInputElement>): void => {
-        const newValue = event.target.value;
+        const element = event.target;
+        const newValue = element.value;
         const cardNumber = newValue.replace(/[^0-9]/g, '');
         const isValid = Payment.fns.validateCardNumber(cardNumber);
+        const cardType = Payment.fns.cardType(cardNumber) as HandledCardTypes;
 
-        setCardNumber(Payment.fns.formatCardNumber(cardNumber));
+        if (cardType && Object.keys(cardLogos).includes(cardType)) {
+            setCardLogo(cardLogos[cardType]);
+        } else {
+            setCardLogo(cardLogos.placeholder);
+        }
+
+        Payment.formatCardNumber(element);
+        setCardNumber(newValue);
 
         if (isValid) {
-            console.log('valid');
             expiryRef.current?.focus();
         }
     };
 
     const onExpiryChange = (event: ChangeEvent<HTMLInputElement>):void => {
-        const newExpiry = event.target.value;
-        const expiryMonthYear = Payment.fns.cardExpiryVal(newExpiry.replace(/[^0-9]/g, ''));
-        setExpiry(`${expiryMonthYear.month}/${expiryMonthYear.year}`);
+        const element = event.target;
+        const newExpiry = element.value;
+        const expiryMonthYear = Payment.fns.cardExpiryVal(newExpiry.replace(/[^0-9/]/g, ''));
+
+        Payment.formatCardExpiry(element);
+
+        if (newExpiry.length === 7) {
+            const isValid = Payment.fns.validateCardExpiry(String(expiryMonthYear.month), String(expiryMonthYear.year));
+            setExpiryValid(isValid);
+
+            if (isValid) {
+                cvcRef.current?.focus();
+            }
+        }
+
+        setExpiry(newExpiry);
+    };
+
+    const onCvcChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const element = event.target;
+        const newCvc = element.value;
+        Payment.formatCardCVC(element);
+        setCvc(newCvc);
     };
 
     return <Wrapper>
-        <Input type="text" onChange={onCardNumberChange} value={cardNumber} />
-        <Input type="text" ref={expiryRef} onChange={onExpiryChange} value={expiry} />
+        <CardLogo src={cardLogo} />
+        <Input
+            type="text"
+            onChange={onCardNumberChange}
+            value={cardNumber}
+            placeholder="Card number"
+        />
+        <Input
+            type="text"
+            ref={expiryRef}
+            onChange={onExpiryChange}
+            value={expiry}
+            width={52}
+            placeholder="MM/YY"
+            maxLength={7}
+            isError={!expiryValid}
+        />
+        <Input
+            type="text"
+            ref={cvcRef}
+            onChange={onCvcChange}
+            value={cvc}
+            width={40}
+            placeholder="CVC"
+            maxLength={4}
+        />
     </Wrapper>
 };
 
